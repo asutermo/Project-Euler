36af7c3275f6ef6490968760c8ee656bc08b0be1
asutermo <asutermo@mymail.mines.edu>
2013-01-09 13:42:07 -0700

    finished problem 10

---
+++ Problem 10/main.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	const int MAX = 2000000;
+	bool prime = true;
+	int numberRoot;
+
+	long long primeSum = 0;
+
+	for (int i = 2; i < MAX; i++) {
+		numberRoot = sqrt(i);
+		for (int j = 2; j <= numberRoot; j++) {
+			if (i % j == 0) {
+				prime = false;
+				break;
+			}		
+		}	
+		if (prime) {
+			primeSum += i;
+			cout << i << endl;
+		}
+		else {
+			prime = true;
+		}
+	}
+	cout << "Prime sum " << primeSum << endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
