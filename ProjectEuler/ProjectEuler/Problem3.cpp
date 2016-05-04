2c1cfd5c2d2005cfd27d9b547a9ce4c74b9b0bd7
Andrew Suter-Morris (TUCKER) <ansuter@microsoft.com>
2016-05-03 21:28:47 -0700

    fix problem 3 slowness

--- ProjectEuler/ProjectEuler/Problem3.cpp
+++ ProjectEuler/ProjectEuler/Problem3.cpp
@@ -1,5 +1,7 @@
 #include "Problem.h"
 #include "ProblemHelpers.h"
+#include <vector>
+using namespace std;
 
 long long ProblemThree()
 {
@@ -7,14 +9,32 @@ long long ProblemThree()
     unsigned long long largest_prime = 0;
     unsigned long long i = 2;
     unsigned long long pf_check = 2;
+    bool foundLargestPrimeFactor = false;
+
+    std::vector<unsigned long long> primes;
 
     while (i < num)
     {
-        if (PrimeFactor(i) && num % i == 0)
+        if (num % i == 0 && PrimeFactor(i))
         {
             largest_prime = i;
-            cout << i << " ";
+            primes.push_back(largest_prime);
         }
+
+        //check to see if all factors accounted for
+        unsigned long long mult = 1;
+        for (size_t j = 0; j < primes.size(); ++j)
+        {
+           mult *= primes[j];
+           if (mult == num)
+           {
+               foundLargestPrimeFactor = true;
+               break;
+           }
+        }
+
+        if (foundLargestPrimeFactor)
+            break;
         ++i;
     }
     return largest_prime;
