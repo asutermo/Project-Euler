2280295453170117ce6a2228abcf48ff4b380e49
Andrew Suter-Morris (TUCKER) <ansuter@microsoft.com>
2016-05-03 22:38:20 -0700

    problem 4 is mucho faster

--- ProjectEuler/ProjectEuler/Problem4.cpp
+++ ProjectEuler/ProjectEuler/Problem4.cpp
@@ -1,16 +1,46 @@
 #include "Problem.h"
 #include "ProblemHelpers.h"
+#include <vector>
+using namespace std;
+
+bool compare(int number)
+{
+    int newNum;
+    std::vector<int> nums;
+    while (number > 0)
+    {
+        newNum = number % 10;
+        int newnewNum = number / 10;
+        nums.push_back(newNum);
+        number = newnewNum;
+        
+    }
+
+    bool isPalindrome = true;
+    int midPoint = nums.size() / 2;
+    for (size_t i = 0; i < midPoint; ++i)
+    {
+        if (nums[i] != nums[nums.size() - i - 1])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
+
+}
 
 int ProblemFour() {
     int maxPalindrome = 0;
-    for (int i = 999; i >= 100; i--) 
+    for (int i = 999; i >= 100; i--)
     {
-        for (int j = 999; j >= 100; j--) 
+        for (int j = 999; j >= 100; j--)
         {
             int number = i * j;
-            string possiblePalindrome = ConvertInt(number);
-            string reverse = ReverseString(possiblePalindrome);
-            if (possiblePalindrome.compare(reverse) == 0)
+
+            //divide and modulo number
+            if (compare(number))
             {
                 if (number > maxPalindrome) {
                     maxPalindrome = number;
